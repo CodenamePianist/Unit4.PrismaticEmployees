78b12443c45325f8cfe0b41b394167f3a543590f
Brandon Hamilton <bham2015@gmail.com>
2024-10-10 12:57:56 -0500

    Added prisma to the package.json file.

---
+++ prisma/seed.js
@@ -0,0 +1,14 @@
+const prisma = require("../prisma");
+const seed = async () => {
+  const employees = [];
+  for (let i = 0; i < 10; i++) {
+    employees.push({ id: i, name: `Employee ${i}` });
+  }
+};
+seed()
+  .then(async () => await prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
