15a9b278ccf6a2400dd8df0c2ee9604494344943
Brandon Hamilton <bham2015@gmail.com>
2024-10-11 13:31:52 -0500

    Created and tested logic for the POST endpoint.

--- api/employees.js
+++ api/employees.js
@@ -2,12 +2,28 @@ const express = require("express");
 const router = express.Router();
 module.exports = router;
 
-const prisma = require("../prisma");
+const prisma = require("..");
 
 router.get("/", async (req, res, next) => {
   try {
     const employees = await prisma.employee.findMany();
     res.json(employees);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  const { name } = req.body;
+  if (!name) {
+    return next({
+      status: 400,
+      message: "New employee needs a name.",
+    });
+  }
+  try {
+    const employee = await prisma.employee.create({ data: { name } });
+    res.status(201).json(employee);
   } catch (error) {
     next(error);
   }
