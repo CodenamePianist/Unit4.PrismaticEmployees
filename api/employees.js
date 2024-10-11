61b01075d94387ac48b8185f1bc05816aa9c7173
Brandon Hamilton <bham2015@gmail.com>
2024-10-11 12:57:57 -0500

    Added GET logic for the /employees endpoint in employees.js.

--- api/employees.js
+++ api/employees.js
@@ -3,3 +3,12 @@ const router = express.Router();
 module.exports = router;
 
 const prisma = require("../prisma");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const employees = await prisma.employee.findMany();
+    res.json(employees);
+  } catch (error) {
+    next(error);
+  }
+});
